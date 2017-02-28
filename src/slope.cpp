d7b0eccd52480aaebca0f07920193dbc2ebe8fee
Clownacy <Clownacy@users.noreply.github.com>
2017-02-28 21:48:35 +0000

    After playing around in Arthur's House, I think this is how it's done

--- src/slope.cpp
+++ src/slope.cpp
@@ -307,7 +307,6 @@ char blocked_wall;
 	if (blocked_wall)
 	{	// we can't actually move...so reset Y position
 		o->y = oldy;
-		o->xinertia = 0;
 	}
 	else
 	{	// can move...complete the move by setting the X position too
